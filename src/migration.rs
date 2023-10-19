89bd691283eba57bf106c705f2a0ea95da02e1ae
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-10-19 22:35:39 +0900

    マイグレ終わったらmemcachedしばく

--- src/migration.rs
+++ src/migration.rs
@@ -1,11 +1,17 @@
 use std::{
-    env,
+    env::{self, VarError},
     fs::File,
     process::{Command, Stdio},
 };
 
 pub fn insert_data(generated_sql_path: String) -> Result<(), Box<dyn std::error::Error>> {
     let generated_sql_file = File::open(generated_sql_path)?;
+
+    let memcached_url = match env::var("MEMCACHED_URL") {
+        Ok(s) => s,
+        Err(VarError::NotPresent) => panic!("$MEMCACHED_URL is not set."),
+        Err(VarError::NotUnicode(_)) => panic!("$MEMCACHED_URL should be written in Unicode."),
+    };
 
     Command::new("mysql")
         .arg(format!("-u{}", env::var("MYSQL_USER").unwrap()))
@@ -16,6 +22,9 @@ pub fn insert_data(generated_sql_path: String) -> Result<(), Box<dyn std::error:
         .stdin(Stdio::from(generated_sql_file))
         .output()
         .expect("failed to import generated sql file");
+
+    let cache_client = memcache::connect(memcached_url).unwrap();
+    cache_client.flush().unwrap();
 
     Ok(())
 }
