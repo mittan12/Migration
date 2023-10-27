9cba265a999fa6abbab7e09f93016ba3c97335f7
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-10-27 13:40:48 +0900

    お前何年非同期プログラミングやってきたんだよ

--- src/migration.rs
+++ src/migration.rs
@@ -8,7 +8,7 @@ pub fn insert_data(generated_sql_path: String) -> Result<(), Box<dyn std::error:
     let generated_sql_file = File::open(generated_sql_path)?;
     let memcached_url = env::var("MEMCACHED_URL")?;
 
-    Command::new("mysql")
+    let mut child = Command::new("mysql")
         .arg(format!("-u{}", env::var("MYSQL_USER").unwrap()))
         .arg(format!("-p{}", env::var("MYSQL_PASSWORD").unwrap()))
         .arg(format!("-S{}", env::var("MYSQL_SOCKET").unwrap()))
@@ -16,6 +16,8 @@ pub fn insert_data(generated_sql_path: String) -> Result<(), Box<dyn std::error:
         .arg(env::var("MYSQL_DATABASE").unwrap())
         .stdin(Stdio::from(generated_sql_file))
         .spawn()?;
+    let exit_status = child.wait()?;
+    println!("{}", exit_status);
 
     let cache_client = memcache::connect(memcached_url)?;
     cache_client.flush()?;
